41bb19d30d05d77608a27180c2cf00838196765d
Satyam06 <satyampandey.58@gmail.com>
2020-09-14 00:21:14 +0200

    Update Monitor_Latency.py

--- Monitor_Latency.py
+++ Monitor_Latency.py
@@ -7,51 +7,65 @@ Created on Tue Sep  8 07:38:55 2020
 from tkinter import *
 import cv2 as cv
 import time
+import csv
+import statistics as st
 import keyboard
 import numpy as np
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
-### for a variable size full screen blue display
+i2c = busio.I2C (board.SCL, board.SDA)
+ads = ADS.ADS1115 (i2c)
+voltageMax = 3.3
+chan0 = AnalogIn (ads, ADS.P0)
+thresholdRes = 100
+Monitor_name = str(input("Please enter a name of your current display "))
+                   
+
 screen_size=Tk()
 width=screen_size.winfo_screenwidth()
 height=screen_size.winfo_screenheight()
 screen_size.geometry("%dx%d+0+0" %(width,height))
-screen_size.configure(background='blue')
+screen_size.configure(background='black')
 start_time=0
+list_time=[]
 
 def Screen_detection():
-    video = cv.VideoCapture(0)
-    check, frame = video.read()  
-    cv.imshow("Window", frame)
-    video.release()
-    cv.waitKey(0)
-    img=cv.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))       
-    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)         
-    lower_range = np.array([0,100,100])
-    upper_range = np.array([2,255,255])     
-    mask = cv.inRange(hsv, lower_range, upper_range)     
-    cv.imshow('image', img)
-    cv.imshow('mask', mask)    
-    exec_time = time.time() - start_time    
-    cv.waitKey(0)
-    cv.destroyAllWindows()    
-    print("Execution time is ",exec_time)
+    
+    
+    resistance = thresholdRes + 1 # dummy condition
+    while resistance > thresholdRes:
+        resistanceRaw = chan0.voltage / (voltageMax - chan0.voltage) * 10
+        if resistanceRaw > 0:
+            resistance = resistanceRaw            
+        time.sleep(0.001)
+        #print ("Voltage value:", chan0.voltage, "Resistance value:", resistance)
+
+    exec_time = time.time() - start_time
+    list_time.append(exec_time)
+       
+    print("Execution time is ",exec_time)    
+    wr_time=time.strftime('%d-%m-%Y %H:%M:%S')
+    avg_exec = str(st.mean(list_time))
+    with open('Data_logger.csv','a') as data_log:
+        logger = csv.writer(data_log)
+        logger.writerow([Monitor_name,wr_time,avg_exec])
+    
+    data_log.close()
         
     
-### clocks the time at which the display data is sent with 's' button pressed and Camera ON
-### this
 while True:
-    if(keyboard.is_pressed('s')):    
-        print("Displaying the blank screen now")
+
+        print("Displaying the white screen now")
+        screen_size.configure(background='white')
+        screen_size.update()
         start_time = time.time()
-        #screen_size.mainloop()
         Screen_detection()
-    else:
-        print("Press s to start")
-        time.sleep(5)
+        print("Displaying the black screen now")
+        screen_size.configure(background='black')
+        screen_size.update()
+        time.sleep(1)
         #break
         
-
-### clocks the time at which the white screen is detected by the rPi sensor with green LED blink
-#exec_time = time.time() - start_time
-#print("Execution time is ",exec_time)
-
