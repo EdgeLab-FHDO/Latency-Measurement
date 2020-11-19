89791e21685bc378d596f692074ecc901af2e893
Satyam06 <satyampandey.58@gmail.com>
2020-11-19 23:15:42 +0100

    Update Rpi time sync with a Switch.cpp

--- Rpi time sync with a Switch.cpp
+++ Rpi time sync with a Switch.cpp
@@ -19,6 +19,14 @@ int main() {
             gettimeofday(&now, NULL);
             //cout<<"time_sec "<<now.tv_sec <<endl;
             //cout<<"Local date and time is "<<now.tv_usec <<endl;
+            
+            //struct timespec ts;
+            //timespec_get(&ts, TIME_UTC);
+            //char buff[100];
+            //strftime(buff, sizeof buff, "%D %T", gmtime(&ts.tv_sec));
+            //printf("Current time: %s.%09ld UTC\n", buff, ts.tv_nsec);
+            //delay(2000);
+            
             ofstream file;
             file.open("data_file.txt",ios::app);
             file<<now.tv_usec <<endl;
